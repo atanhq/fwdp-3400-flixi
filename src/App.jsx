7c9c8f3c4e36d815eaa48d47bc5660f79ef512fe
amt <mt@mt-2.local>
2026-01-23 10:59:58 -0800

    added base folders and styles to main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/styles.css'
+import './styles/normalize-fwd.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>H1 Heading</h1>
+      <h2>H2 Heading</h2>
+      <p>Paragraph Text</p>
+    </>
+  )
+}
+
+export default App
