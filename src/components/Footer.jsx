be53ab3df7db729989780d3da824b018b1717573
Alex Monro <alexmonro546@gmail.com>
2026-01-26 22:18:54 -0800

    Working Basic Navigation and footer no interactions yet

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2026 Flixi</p>
+      <p>For educational use only</p>
+    </footer>
+  );
+};
+
+export default Footer;
