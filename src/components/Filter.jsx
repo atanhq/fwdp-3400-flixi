c0946ec4684b32ce41d76d8b7c179fc17d6fd84a
Faith Ann <flumba@my.bcit.ca>
2026-02-09 05:10:02 -0800

    Added pill buttons for tablet and mobile  Still need to fix filter dropdown for mobile and fix her description padding for tablet (CLOSE TO COMPLETION)

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,14 +2,26 @@
 
 function Filter() {
   return (
+    <>
      <div className="filter-container">
         <label className="filter-label">Filter:</label>
         <select className="filter-dropdown">
-          <option value="popular">Popular</option>
           <option value="upcoming">Upcoming</option>
-          <option value="new-release">New Release</option>
+          <option value="top-rated">Top Rated</option>
+          <option value="popular">Popular</option>
+           <option value="now-playing">Now Playing</option>
         </select>
       </div>
+
+      {/* Filter Pills*/}
+      <div className="filter-pills">
+        <button className="pill-button active">Upcoming</button>
+        <button className="pill-button ">Top Rated</button>
+        <button className="pill-button ">Popular</button>
+        <button className="pill-button ">Now Playing</button>
+
+      </div>
+    </>
   );
 }
 
